2c7e31e595aed9f31193d6e4f2cb88e55f2c35b1
groundlevelgeek <groundlevelgeek@gmail.com>
2021-07-18 15:33:03 +0530

    Adds Insertion sort for unsorted linear dsa

---
+++ src/main/java/org/groundlevelgeek/InsertionSort/InsertionSort.java
@@ -0,0 +1,36 @@
+package org.groundlevelgeek.InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{20, 24, 35, 40, 25, 15, 7, -15, 55, 1, -22};
+        System.out.println("\n\n");
+        Arrays.stream(array).mapToObj(element -> element + ", ").forEach(System.out::print);
+
+        // algorithm
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
+            var unsortedElementIndex = firstUnsortedIndex;
+            var noSwap = false;
+            for (int index = firstUnsortedIndex - 1; index > -1; index--) {
+                if (array[unsortedElementIndex] < array[index]) {
+                    var holder = array[index];
+                    array[index] = array[unsortedElementIndex];
+                    array[unsortedElementIndex] = holder;
+                    unsortedElementIndex = index;
+                    noSwap = true;
+                }
+                if (!noSwap) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\n\n");
+        Arrays.stream(array).mapToObj(element -> element + ", ").forEach(System.out::print);
+
+    }
+
+}
